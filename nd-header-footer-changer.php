1fa1f6f531d5dcaa48c65887a67d701be9ece1b0
Thiha Tin Maung Aye <im@thiha.me>
2013-09-13 14:00:54 +0630

    Create

---
+++ nd-header-footer-changer.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Plugin Name: Nomesdesign Header Footer Change
+ * Plugin URI: http://blog.nomesdesign.com
+ * Description: This plugin is for changing Header Banner and Footer Banner easily.
+ * Author: Thiha (Synapseoriginal)
+ * Version: 1.0
+ * Author URI: http://synapseoriginal.com
+ */
